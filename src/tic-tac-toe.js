c151dae412b57001d9d8e421df6a19dc5dd498c3
Nikolay <n.smirnov.kz@gmail.com>
2019-09-25 11:18:19 +0600

    submit

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,65 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
+    this.field = [[null, null, null], [null, null, null], [null, null, null]];
+    this.currentPlayer = "x";
+  }
 
+  getCurrentPlayerSymbol() {
+    return this.currentPlayer;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    if (!this.field[rowIndex][columnIndex]) {
+      this.field[rowIndex][columnIndex] = this.currentPlayer;
+      this.currentPlayer = this.currentPlayer === "x" ? "o" : "x";
+    }
+  }
+
+  isFinished() {
+    return Boolean(this.getWinner() || this.isDraw());
+  }
+
+  checkForWin(player) {
+    const buffers = [];
+
+    for (let i = 0; i < this.field.length; i++) {
+      const horizontal = [];
+      const vertical = [];
+      for (let j = 0; j < this.field.length; j++) {
+        horizontal.push(this.field[i][j]);
+        vertical.push(this.field[j][i]);
+      }
+      buffers.push(horizontal, vertical);
     }
 
-    getCurrentPlayerSymbol() {
-
+    const leftDiagonal = [];
+    const rightDiagonal = [];
+    for (let i = 0; i < this.field.length; i++) {
+      leftDiagonal.push(this.field[i][i]);
+      rightDiagonal.push(this.field[i][this.field.length - i - 1]);
     }
+    buffers.push(leftDiagonal, rightDiagonal);
 
-    nextTurn(rowIndex, columnIndex) {
+    return buffers.some(line => line.every(value => value === player));
+  }
 
-    }
+  getWinner() {
+    if (this.checkForWin("x")) return "x";
+    if (this.checkForWin("o")) return "o";
+    return null;
+  }
 
-    isFinished() {
+  noMoreTurns() {
+    return this.field.every(line => line.every(value => Boolean(value)));
+  }
 
-    }
+  isDraw() {
+    return Boolean(this.noMoreTurns() && !this.getWinner());
+  }
 
-    getWinner() {
-
-    }
-
-    noMoreTurns() {
-
-    }
-
-    isDraw() {
-
-    }
-
-    getFieldValue(rowIndex, colIndex) {
-
-    }
+  getFieldValue(rowIndex, colIndex) {
+    return this.field[rowIndex][colIndex];
+  }
 }
 
 module.exports = TicTacToe;
